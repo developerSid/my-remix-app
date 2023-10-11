28c7027eed8395425b6b8cc27a2b0d610b2772bf
developerSid <developerSid@gmail.com>
2023-10-11 09:29:35 -0500

    Updated _index.tsx to make a call to the joke api

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { defer } from "@remix-run/cloudflare";
+import { Await, useLoaderData, useFetcher } from "@remix-run/react";
+import { Suspense } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +10,66 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+enum JokeType {
+  single = "single",
+  twopart = "twopart",
+}
+
+type SingleJoke = {
+  type: JokeType.single;
+  joke: string;
+};
+
+type TwopartJoke = {
+  type: JokeType.twopart;
+  setup: string;
+  delivery: string;
+};
+
+type JokeResponse = SingleJoke | TwopartJoke;
+
+const getJoke = async () => {
+  const response = await fetch(
+    "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+  );
+  if (!response.ok) throw new Error(response.statusText);
+  return await (response.json() as Promise<JokeResponse>);
+};
+
+export const loader = async () => {
+  const jokePromise = getJoke();
+  return defer({ jokeData: jokePromise });
+};
+export const action = async () => null;
+
 export default function Index() {
+  const { jokeData } = useLoaderData<typeof loader>();
+  const fetcher = useFetcher();
+  const resolveJoke = (data: JokeResponse) =>
+    data.type == JokeType.single ? (
+      <h3>{data.joke}</h3>
+    ) : (
+      <>
+        <h3>{data.setup}..</h3>
+        <h3>{data.delivery}</h3>
+      </>
+    );
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="container mx-auto rounded shadow-lg outline outline-1 outline-gray-100 m-5">
+      <div className="px-6 py-4">
+        <h1 className="text-3xl font-bold mb-2">Get-A-Joke</h1>
+        <Suspense fallback={<h3>Loading...</h3>}>
+          <Await resolve={jokeData}>{resolveJoke}</Await>
+        </Suspense>
+        <div className="my-2">
+          <fetcher.Form method="post">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+              Another One Please
+            </button>
+          </fetcher.Form>
+        </div>
+      </div>
     </div>
   );
 }
